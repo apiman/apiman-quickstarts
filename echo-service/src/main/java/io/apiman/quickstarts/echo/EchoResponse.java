95141de3a03471cb156a6bbadafa6c1851dba87c
Eric Wittmann <eric.wittmann@gmail.com>
2015-04-22 12:37:40 -0400

    fix warning

--- echo-service/src/main/java/io/apiman/quickstarts/echo/EchoResponse.java
+++ echo-service/src/main/java/io/apiman/quickstarts/echo/EchoResponse.java
@@ -83,7 +83,7 @@ public class EchoResponse {
     private String method;
     private String resource;
     private String uri;
-    private Map<String, String> headers = new HashMap<String, String>();
+    private Map<String, String> headers = new HashMap<>();
     private Long bodyLength;
     private String bodySha1;
     
